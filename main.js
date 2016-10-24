0d2c837a32fcfb8240856225fa4f855471469f80
Heather MacPhail <hmacphail@gmail.com>
2016-10-23 22:13:48 -0700

    Pull album data from XML

---
+++ main.js
@@ -0,0 +1,42 @@
+//static global vars
+var ALBUM_URL = "https://musicbrainz.org/ws/2/release/3611e54a-1a2a-4fdf-af78-287b845e5ebb?inc=aliases%2Bartist-credits%2Bdiscids%2Blabels%2Brecordings";
+
+//on load
+$.get(ALBUM_URL, function(data) {
+  parseAlbumXml($(data));
+});
+
+function parseAlbumXml(dataXml){
+  //album data
+  var artist = dataXml.find("release>artist-credit name"),
+    album = dataXml.find("release>title"),
+    releaseDate = dataXml.find("release release-event>date"),
+    label = dataXml.find("release label>name");
+
+  //display album data
+  var albumInfoDiv = $("#album-info");
+  albumInfoDiv.append("<div>" + artist.text() + "</div>")
+    .append("<div>" + album.text() + "</div>")
+    .append("<div>" + releaseDate.text() + "</div>")
+    .append("<div>" + label.text() + "</div>");
+
+  //track list
+  var tracksArray = dataXml.find("track-list track");
+
+  var trackListDiv = $("#track-list tbody");
+  for (var ii=0; ii<tracksArray.length; ii++) {
+      var track = $(tracksArray[ii]);
+
+      var position = track.find("track>position"),
+        title = track.find("track>recording>title"),
+        artist = track.find("track>recording>artist-credit name"),
+        duration = track.find("track>length");
+
+      trackListDiv.append("<tr>")
+        .append("<td>" + position.text() + "</td>")
+        .append("<td>" + title.text() + "</td>")
+        .append("<td>" + artist.text() + "</td>")
+        .append("<td>" + duration.text() + "</td>")
+        .append("</tr>");
+  }
+}
