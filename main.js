80ba62ae553b95d5d26bec436e3dc5c1dd1da6c1
Heather MacPhail <hmacphail@gmail.com>
2016-10-23 22:32:13 -0700

    Date/time formatting

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //static global vars
 var ALBUM_URL = "https://musicbrainz.org/ws/2/release/3611e54a-1a2a-4fdf-af78-287b845e5ebb?inc=aliases%2Bartist-credits%2Bdiscids%2Blabels%2Brecordings";
+var COVER_ART = "";
 
 //on load
 $.get(ALBUM_URL, function(data) {
@@ -13,11 +14,14 @@ function parseAlbumXml(dataXml){
     releaseDate = dataXml.find("release release-event>date"),
     label = dataXml.find("release label>name");
 
+  var date = new Date(releaseDate.text()),
+    dateOptions = {year: "numeric", month: "short", day: "numeric"};
+
   //display album data
   var albumInfoDiv = $("#album-info");
   albumInfoDiv.append("<div>" + artist.text() + "</div>")
     .append("<div>" + album.text() + "</div>")
-    .append("<div>" + releaseDate.text() + "</div>")
+    .append("<div>" + date.toLocaleDateString("en-US", dateOptions) + "</div>")
     .append("<div>" + label.text() + "</div>");
 
   //track list
@@ -25,18 +29,22 @@ function parseAlbumXml(dataXml){
 
   var trackListDiv = $("#track-list tbody");
   for (var ii=0; ii<tracksArray.length; ii++) {
-      var track = $(tracksArray[ii]);
+    var track = $(tracksArray[ii]);
 
-      var position = track.find("track>position"),
-        title = track.find("track>recording>title"),
-        artist = track.find("track>recording>artist-credit name"),
-        duration = track.find("track>length");
+    var position = track.find("track>position"),
+      title = track.find("track>recording>title"),
+      artist = track.find("track>recording>artist-credit name"),
+      duration = track.find("track>length");
 
-      trackListDiv.append("<tr>")
-        .append("<td>" + position.text() + "</td>")
-        .append("<td>" + title.text() + "</td>")
-        .append("<td>" + artist.text() + "</td>")
-        .append("<td>" + duration.text() + "</td>")
-        .append("</tr>");
+    var min = (duration.text()/1000/60) << 0,
+      sec = (duration.text()/1000) % 60 << 0;
+
+    //display each track
+    trackListDiv.append("<tr>")
+      .append("<td>" + position.text() + "</td>")
+      .append("<td>" + title.text() + "</td>")
+      .append("<td>" + artist.text() + "</td>")
+      .append("<td>" + min + ':' + sec + "</td>")
+      .append("</tr>");
   }
 }
