bf579d8d1700010bf9fabe723e22b943e78212fa
Heather MacPhail <hmacphail@gmail.com>
2016-10-24 15:30:44 -0700

    Page alignment and styling

--- main.js
+++ main.js
@@ -1,50 +1,51 @@
 //static global vars
 var ALBUM_URL = "https://musicbrainz.org/ws/2/release/3611e54a-1a2a-4fdf-af78-287b845e5ebb?inc=aliases%2Bartist-credits%2Bdiscids%2Blabels%2Brecordings";
-var COVER_ART = "";
+var COVER_ART = "Eureka.jpg";
 
 //on load
 $.get(ALBUM_URL, function(data) {
   parseAlbumXml($(data));
 });
 
+$("#album-cover").append("<img src='" + COVER_ART + "'>")
+
 function parseAlbumXml(dataXml){
   //album data
-  var artist = dataXml.find("release>artist-credit name"),
-    album = dataXml.find("release>title"),
-    releaseDate = dataXml.find("release release-event>date"),
-    label = dataXml.find("release label>name");
+  var album = dataXml.find("release>title").text(),
+    artist = dataXml.find("release>artist-credit name").text(),
+    releaseDate = dataXml.find("release release-event>date").text(),
+    label = dataXml.find("release label>name").text();
 
-  var date = new Date(releaseDate.text()),
+  var date = new Date(releaseDate),
     dateOptions = {year: "numeric", month: "short", day: "numeric"};
 
   //display album data
   var albumInfoDiv = $("#album-info");
-  albumInfoDiv.append("<div>" + artist.text() + "</div>")
-    .append("<div>" + album.text() + "</div>")
-    .append("<div>" + date.toLocaleDateString("en-US", dateOptions) + "</div>")
-    .append("<div>" + label.text() + "</div>");
+  albumInfoDiv.append("<div id='album-name'>" + album + "</div>")
+    .append("<div id='album-artist'>" + artist + "</div>")
+    .append("<div id='release-date'>" + date.toLocaleDateString("en-US", dateOptions) + "</div>")
+    .append("<div id='record-label'>" + label + "</div>");
 
   //track list
   var tracksArray = dataXml.find("track-list track");
-
-  var trackListDiv = $("#track-list tbody");
   for (var ii=0; ii<tracksArray.length; ii++) {
     var track = $(tracksArray[ii]);
 
-    var position = track.find("track>position"),
-      title = track.find("track>recording>title"),
-      artist = track.find("track>recording>artist-credit name"),
-      duration = track.find("track>length");
+    var position = track.find("track>position").text(),
+      title = track.find("track>recording>title").text(),
+      artist = track.find("track>recording>artist-credit name").text(),
+      duration = track.find("track>length").text();
 
-    var min = (duration.text()/1000/60) << 0,
-      sec = (duration.text()/1000) % 60 << 0;
+    var min = (duration/1000/60) << 0,
+      sec = (duration/1000) % 60 << 0;
 
     //display each track
-    trackListDiv.append("<tr>")
-      .append("<td>" + position.text() + "</td>")
-      .append("<td>" + title.text() + "</td>")
-      .append("<td>" + artist.text() + "</td>")
-      .append("<td>" + min + ':' + sec + "</td>")
-      .append("</tr>");
+    var trackRow = "<tr>"
+      + "<td class='position'>" + position + "</td>"
+      + "<td>" + title + "</td>"
+      + "<td>" + artist + "</td>"
+      + "<td class='duration'>" + min + ":" + sec + "</td>"
+      + "</tr>";
+    $("#track-list tbody").append(trackRow);
   }
 }
